8995a1cd0b95a06c5c3563c00273abe91f9727b4
Thomas Le Sciellour <thomas.lesciellour@gmail.com>
2018-04-05 18:45:41 +0200

    Insert New list

---
+++ www/js/components.js
@@ -0,0 +1,56 @@
+window.addEventListener('load', function() {
+
+  // Définition des composants
+  Vue.component('item', {
+    template: '#item',
+    props: [ 'elt' ],
+    updated: function() {
+      console.log('Item updated');
+    },
+    methods: {
+      getItem: function() {
+        return {
+          value: this.elt.value,
+          checked: this.elt.checked
+        };
+      },
+      saveTick: function(e) {
+        var title = this.elt.value,
+            checked = e.target.checked;
+
+        this.elt.checked = checked;
+        this.$emit('update:elt', this.elt);
+      }
+    }
+  });
+
+  Vue.component('todo-list', {
+    template: '#todoList',
+    props: [ 'title', 'contents' ],
+    data: function() {
+      return {
+        elt: {
+          title: this.title,
+          contents: this.contents
+        }
+      };
+    },
+    updated: function() {
+      console.log('List Updated');
+    },
+    methods: {
+      updateTitle: function(elt) {
+        this.$emit('update:title', elt.target.value);
+      },
+      saveNewItem: function() {
+        var elt = document.getElementById('in_new_element');
+
+        this.contents.push({
+          value: elt.value
+        });
+        elt.value = '';
+      }
+    }
+  });
+
+});
--- www/js/database.js
+++ www/js/database.js
@@ -1,21 +1,83 @@
 var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB;
+var DatabaseORM = new ORM();
 
-var open = indexedDB.open("Keeper", 1);
 
+function ORM() {
+  
+  var open = indexedDB.open("Keeper", 1);
+  var that = this;
 
-// Create the schema
-open.onupgradeneeded = function() {
-    var db = open.result;
-    var store = db.createObjectStore("List", { keyPath: "id", autoIncrement: true });
-    
-    store.createIndex("title", "title", { unique: false });
-    store.createIndex("contents", "contents", { unique: false });
+  // Create the schema
+  open.onupgradeneeded = function() {
+      that.db = open.result;
+      var store = that.db.createObjectStore("List", { keyPath: "id", autoIncrement: true });
+      
+      store.createIndex("title", "title", { unique: false });
+      store.createIndex("contents", "contents", { unique: false });
+  };
+
+  open.onsuccess = function() {
+    that.db = open.result;
+    console.log('Database success');
+  };
+
+  open.onerror = function(err) {
+    console.error('Database error');
+    console.error(err);
+  };
+
+}
+
+ORM.prototype.insertList = function(title, contents) {
+  var tx = this.db.transaction(["List"], "readwrite"),
+      store = tx.objectStore("List");
+
+  tx.oncomplete = function(event) {
+    console.log("All done!");
+  };
+
+  tx.onerror = function(error) {
+    console.error(error)
+  };
+
+  store.put({title: title, contents: JSON.stringify(contents) });
 };
 
-open.onsuccess = function() {
-  var db = open.result;
-  var tx = db.transaction("List", "readwrite");
-  var store = tx.objectStore("List");
-  
-  console.log(store.getAll());
+ORM.prototype.getAllLists = function(fn) {
+  if (this.db) {
+    var transaction = this.db.transaction(["List"]),
+        objectStore = transaction.objectStore("List"),
+        request = objectStore.getAll();
+    
+    request.onerror = function(err) {
+      console.error('Request Error');
+    };
+    
+    request.onsuccess = function() {
+      var result = request.result;
+
+      for (var i=0; i<result.length; i++) {
+        result[i].contents = JSON.parse(result[i].contents);
+      }
+
+      if (fn) {
+        fn(result);
+      }
+    };
+  } else {
+    var that = this;
+    setTimeout(function() {
+      that.getAllLists(fn);
+    }, 200);
+  }
+};
+
+ORM.prototype.getLastList = function(fn) {
+  this.getAllLists(function(lists) {
+    if (lists && lists.length > 0) {
+      fn(lists[lists.length-1]);
+    } else {
+      fn(false);
+    }
+  })
 };
