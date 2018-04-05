efd66d6955699be3682de1329c784935dc6c2d2e
Thomas Le Sciellour <thomas.lesciellour@gmail.com>
2018-04-05 16:17:49 +0200

    Update icon

--- www/js/service-worker.js
+++ www/js/service-worker.js
@@ -1,61 +1,40 @@
-const CACHE_NAME = 'keeper-cache',
-      CACHE_FILES = [
-          '/',
-          '/index.html',
-          '/manifest.json',
-          '/js/vue.js',
-          '/js/materialize.js',
-          '/js/app.js',
-          '/js/service-worker.js',
-          '/css/materialize.min.css',
-          '/css/app.css',
-          '/fonts/MaterialIcons-Regular.eot',
-          '/fonts/MaterialIcons-Regular.svg',
-          '/fonts/MaterialIcons-Regular.woff2',
-          '/fonts/MaterialIcons-Regular.woff',
-          '/fonts/MaterialIcons-Regular.ttf',
-          '/fonts/MaterialIcons-Regular.ijmap'
-        ];
-
-// Install Cache
-self.addEventListener('install', function(e) {
-  console.log('Cache Installation');
-
-  e.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(CACHE_FILES);
-      })
-      .catch(function(err) {
-        console.error('Error to install the application cache');
-      })
-  );
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activating');  
-});
-
-// Offline Response
-self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-
-  e.respondWith(
-    caches
-      .match(e.request)
-      .then(function(res) {
-        return res || fetch(e.request);
-      })
-      .catch(function(err) {
-        console.error('Error to rend ' + e.request.url);
-      })
-  );
-});
-
-/*
 (function(Config) {
 
+  // Install Cache
+  self.addEventListener('install', function(e) {
+    console.log('Cache Installation');
+
+    e.waitUntil(
+      caches
+        .open(Config.CACHE_NAME)
+        .then(function(cache) {
+          return cache.addAll(Config.CACHE_FILES);
+        })
+        .catch(function(err) {
+          console.error('Error to install the application cache');
+        })
+    );
+  });
+
+  self.addEventListener('activate', function(event) {
+    console.log('Service Worker activating');  
+  });
+
+  // Offline Response
+  self.addEventListener('fetch', function(e) {
+    console.log(e.request.url);
+
+    e.respondWith(
+      caches
+        .match(e.request)
+        .then(function(res) {
+          return res || fetch(e.request);
+        })
+        .catch(function(err) {
+          console.error('Error to rend ' + e.request.url);
+        })
+    );
+  });
 
 })({
 
@@ -82,4 +61,3 @@ self.addEventListener('fetch', function(e) {
   ]
 
 });
-*/
